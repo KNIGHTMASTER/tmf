5c70bd825e67fdcd6cbf1e8a043c6a7ba8b9cb1a
KNIGHTMASTER <fauzi.knightmaster.achmad@gmail.com>
2017-09-06 00:28:13 +0700

    Change README + Refactoring

--- dao/src/main/java/id/co/telkomsigma/tmf/dao/ISecurityOAuthClientDetailsDAO.java
+++
@@ -1,9 +0,0 @@
-package id.co.telkomsigma.tmf.dao;
-
-import id.co.telkomsigma.tmf.data.model.security.SecurityOAuthClientDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ISecurityOAuthClientDetailsDAO extends JpaRepository<SecurityOAuthClientDetails, String> {
-	
-	SecurityOAuthClientDetails findByClientId(String p_ClientId);
-}
--- service/src/main/java/id/co/telkomsigma/tmf/service/ISecurityUserContactService.java
+++
@@ -1,7 +0,0 @@
-package id.co.telkomsigma.tmf.service;
-
-import id.co.telkomsigma.tmf.data.model.master.contact.SecurityUserContact;
-import id.co.telkomsigma.tmf.service.scaffolding.IScaffoldingService;
-
-public interface ISecurityUserContactService extends IScaffoldingService<SecurityUserContact> {
-}
